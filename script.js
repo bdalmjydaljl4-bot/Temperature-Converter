261033c15906ff02e7d2c686d17f8825ecf13271
YourName <your@email.com>
2026-03-03 04:07:55 +0300

    Cleaning up old files to upload new code

--- script.js
+++
@@ -1,9 +0,0 @@
-// اختر العناصر
-const box = document.getElementById('box');
-const btn = document.getElementById('btn');
-
-// أضف حدث عند الضغط على الزر
-btn.addEventListener('click', () => {
-    // غيّر لون المربع من أحمر إلى أزرق أو العكس
-    box.style.backgroundColor = box.style.backgroundColor === 'red' ? 'blue' : 'red';
-});
